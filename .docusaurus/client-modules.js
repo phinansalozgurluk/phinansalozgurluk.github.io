b2b208ffbff1cd6d7fe7fd331052738baa2c20c1
phinansalozgurluk <phinansalozgurluk@protonmail.com>
2022-01-23 06:19:05 +0100

    general add

---
+++ .docusaurus/client-modules.js
@@ -0,0 +1,6 @@
+export default [
+  require("/Users/hakandemirel/web/node_modules/infima/dist/css/default/default.css"),
+  require("/Users/hakandemirel/web/node_modules/@docusaurus/theme-classic/lib/prism-include-languages"),
+  require("/Users/hakandemirel/web/node_modules/@docusaurus/theme-classic/lib/admonitions.css"),
+  require("/Users/hakandemirel/web/src/css/custom.css"),
+];
